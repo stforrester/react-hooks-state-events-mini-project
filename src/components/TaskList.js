cba5c0d2295139b44b96045efff75b0000f868ca
Stuart Forrester <stforrester@gmail.com>
2024-12-06 04:00:35 -0600

    completes the TaskList deliverable

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+
+  const tasksList = tasks.map((task => {
+    return (
+      <Task key={task.text} text={task.text} category={task.category} />
+    )
+  }))
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasksList}
     </div>
   );
 }
